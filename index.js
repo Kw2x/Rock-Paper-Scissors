4f78a19201e906ac9ba4621c8ac659e8d1b39d22
kw2x <bhen2014@gmail.com>
2022-09-25 12:19:13 -0400

    finished the getRound function, also added a return string returning winner, created a function that also lower caseses usser input

--- index.js
+++ index.js
@@ -6,36 +6,41 @@ function getComputerChoice () {
     let random = Math.floor(Math.random() * cChoice.length);
     return (cChoice[random]);
 }
-
-// Player Choice
-let pChoice = ['Rock', 'Paper', 'Scissors'];
-function getPlayerChoice(){
-    let random = Math.floor(Math.random() * pChoice.length);
-    return (pChoice[random]);
-}
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-       return `It's a tie! you both picked ${playerSelection}`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-       return "You win! Rock beats Scissors";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-       return "You win! Paper beats Rock";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-       return "You win! Scissors beats Paper";
-    } else {
-       return `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
- }
- 
-
 //# write a function that plays a single round - playRound function should take two parameters -the playerSelection and the computerSelection
 
+// getRound
+function getRound(playerSelection, computerSelection) {
+   if (playerSelection === computerSelection) {
+      return `It's a tie! you both picked ${playerSelection}`;
+   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return "You win! Rock beats Scissors";
+   } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return "You win! Paper beats Rock";
+   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return "You win! Scissors beats Paper";
+   } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+   }
+}
 
-// this dosnt work beaause cChoice and pChoice  are inside an array. 
-
-   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(getRound(playerSelection.toLowerCase(), computerSelection));
 //# then return a string that delcares the winner of the round like - "You Lose! Paper beats Rock"
-//# make your functions playerSelection parameter case senstive so the user can input and variation of - (rock, Rock RoCk)
+else {
+   return `You lose! ${computerSelection} beats ${playerSelection}`;
+}
+//# make your functions playerSelection parameter case insenstive so the user can input and variation of - (rock, Rock RoCk)
+console.log(getRound(playerSelection.toLowerCase(), computerSelection));
 //# IMPORTANT we want to return the of the function call not console.log()
+console.log(getRound(playerSelection.toLowerCase(), computerSelection));
 //# write a new function called game(). it calls the playRound function inside of game()
+function game() {
+      getRound()
+}
 //# create a loop to play 5 rounds 
+function game() {
+   for (let i = 0; i < 5; i++) {
+      getRound()
+   }
+}
