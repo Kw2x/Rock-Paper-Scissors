5a8ab0048d718c8594509f7d2a7133a1a3799480
kw2x <bhen2014@gmail.com>
2022-09-26 22:56:26 -0400

    made edits to the getGame function

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 let playerSelection = prompt('Rock, Paper, Scissors')
 playerSelection = playerSelection.toLowerCase();
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 
-let playerScore=0;
-let computerScore=0;
-let tieScore=0;
+
 console.log(getRound(playerSelection, computerSelection));
 
 function getComputerChoice() {
     let cChoice = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * cChoice.length);
-    return(cChoice[random]);}
+    return(cChoice[random]);
+   }
 
 function getRound(playerSelection, computerSelection) {
-   if (playerSelection === getComputerChoice) {
+   let playerScore= 0;
+   let computerScore= 0;
+   let tieScore= 0;
+
+   if (playerSelection === computerSelection) {
       tieScore++; 
       return `It's a tie! you both picked ${playerSelection}`;
    } else if (playerSelection === "rock" && computerSelection === "scissors") {
       playerScore++;
-      return "Player wins! Rock beats Scissors" + playerScore;
+      return "Player wins! Rock beats Scissors"
    } else if (playerSelection === "paper" && computerSelection === "rock") {
       playerScore++;
-      return "Player wins! Paper beats Rock" + playerScore;
+      return "Player wins! Paper beats Rock" 
    } else if (playerSelection === "scissors" && computerSelection === "paper") {
       playerScore++;
-      return "Player wins! Scissors beats Paper" + playerScore;
+      return "Player wins! Scissors beats Paper" 
    } else {
       computerScore++; 
-      return `You lose! ${playerSelection} beats ${computerSelection}` + computerScore;
+      return `You lose! ${computerSelection} beats ${playerSelection}`
  }
 }
 
 function getGame() {
-   for (let i = 0; i <5; i++){
+   for (let i = 0; i < 5; i++) {
       getRound();
-      if (playerScore>computerScore){
-         return "You Win this game.\n -------";
-       }
-         if (playerScore==computerScore){
-           return "This game is a tie.\n -------";
-         }
-        else{
-         return "You Lose this game.\n -------";
-       }
-   }
-}
+      let score = 0
+      score = score + 1;
+   return (`The score is now ${score + 1}`) 
+   }}
