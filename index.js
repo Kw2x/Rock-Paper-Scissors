33c9eae2f9328c2e9998ef94081542f92811415e
kw2x <bhen2014@gmail.com>
2022-09-27 20:43:05 -0400

    recreating the getGame function and will be adding switch statemens insted of if else and else if

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-let playerSelection = prompt('Rock, Paper, Scissors')
-playerSelection = playerSelection.toLowerCase();
-let computerSelection = getComputerChoice();
-
-
-console.log(getRound(playerSelection, computerSelection));
 
 function getComputerChoice() {
     let cChoice = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * cChoice.length);
-    return(cChoice[random]);
-   }
+    return(cChoice[random]);}
+    
+let playerSelection = prompt('Rock, Paper, Scissors')
+playerSelection = playerSelection.toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(getRound(playerSelection, computerSelection));
 
 function getRound(playerSelection, computerSelection) {
-   let playerScore= 0;
-   let computerScore= 0;
-   let tieScore= 0;
-
+let playerScore= 0;
+let computerScore= 0;
+let tieScore= 0;
    if (playerSelection === computerSelection) {
-      tieScore++; 
+     
       return `It's a tie! you both picked ${playerSelection}`;
    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      playerScore++;
+     
       return "Player wins! Rock beats Scissors"
    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      playerScore++;
+      
       return "Player wins! Paper beats Rock" 
    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      playerScore++;
+     
       return "Player wins! Scissors beats Paper" 
    } else {
-      computerScore++; 
-      return `You lose! ${computerSelection} beats ${playerSelection}`
+     
+    return `You lose! ${computerSelection} beats ${playerSelection}`
  }
 }
 
-function getGame() {
-   for (let i = 0; i < 5; i++) {
-      getRound();
-      let score = 0
-      score = score + 1;
-   return (`The score is now ${score + 1}`) 
-   }}
+function getGame(){
+let playerScore= 0;
+let computerScore= 0;
+let tieScore= 0;
+getRound(playerSelection, computerSelection)
+for (let i = 0; i < 5; i++){
+if() {
+
+}
+ else if(playerSelection > computerSelection){
+  
+ }
+ else{
+  
+ }
+
+}} 
